c31de5fb32aa325433081cbbd04256649e99bf2a
najuzilu <junaluzi@gmail.com>
2020-08-12 11:04:56 +0200

    add proof types

---
+++ sphinxcontrib/pretty_proof/proof_type.py
@@ -0,0 +1,75 @@
+"""
+sphinxcontrib.pretty_proof.proof_type
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+A Proof Sphinx Domain
+:copyright: Copyright 2020 by the QuantEcon team, see AUTHORS
+:licences: see LICENSE for details
+"""
+
+from .directive import ElementDirective
+
+
+class ProofDirective(ElementDirective):
+    """A custom proof directive."""
+
+    name = "proof"
+
+
+class TheoremDirective(ElementDirective):
+    """A custom theorem directive."""
+
+    name = "theorem"
+
+
+class LemmaDirective(ElementDirective):
+    """A custom lemma directive."""
+
+    name = "lemma"
+
+
+class DefinitionDirective(ElementDirective):
+    """A custom definition directive."""
+
+    name = "definition"
+
+
+class RemarkDirective(ElementDirective):
+    """A custom remark directive."""
+
+    name = "remark"
+
+
+class ConjectureDirective(ElementDirective):
+    """A custom conjecture directive."""
+
+    name = "conjecture"
+
+
+class CorollaryDirective(ElementDirective):
+    """A custom corollary directive."""
+
+    name = "corollary"
+
+
+class AlgorithmDirective(ElementDirective):
+    """A custom algorithm directive."""
+
+    name = "algorithm"
+
+
+class CriteriaDirective(ElementDirective):
+    """A custom criteria directive."""
+
+    name = "criteria"
+
+
+class AxiomDirective(ElementDirective):
+    """A custom axiom directive."""
+
+    name = "axiom"
+
+
+class ExerciseDirective(ElementDirective):
+    """A custom exercise directive."""
+
+    name = "exercise"
