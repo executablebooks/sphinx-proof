4aff73c120f99697ef9bade550ad6aa46ee55b87
najuzilu <junaluzi@gmail.com>
2020-08-18 11:35:31 +0200

    add proof index

--- sphinxcontrib/pretty_proof/domain.py
+++ sphinxcontrib/pretty_proof/domain.py
@@ -1,13 +1,18 @@
 """
 sphinxcontrib.pretty_proof.domain
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 A Proof Sphinx Domain
+
 :copyright: Copyright 2020 by the QuantEcon team, see AUTHORS
 :licences: see LICENSE for details
 """
-from typing import List, Tuple
+from typing import Any, Dict, Tuple, List
 from docutils.nodes import Element, Node, document, system_message
 from sphinx.environment import BuildEnvironment
+from sphinx.addnodes import pending_xref
+from sphinx.builders import Builder
+
 from collections import defaultdict
 from sphinx.domains import Domain, Index
 from sphinx.roles import XRefRole
@@ -15,6 +20,8 @@ from sphinx.util.nodes import make_refnode
 from sphinx.util import logging
 from docutils import nodes
 from .proof_type import *
+
+import pdb
 
 logger = logging.getLogger(__name__)
 
@@ -24,18 +31,25 @@ class ProofIndex(Index):
     localname = "Proof Index"
     shortname = "Proof"
 
-    def generate(self, docnames=None):
+    def generate(self, docnames=None) -> Tuple[Dict[str, Any], bool]:
         content = defaultdict(list)
+        proofs = self.domain.env.proof_list
+
+        # name, subtype, docname, typ, anchor, extra, qualifier, description
+        for anchor, values in proofs.items():
+            content[anchor].append(
+                (anchor, values["prio"], values["docname"], anchor,
+                values["docname"], "", values["type"])
+            )
+
+        content = sorted(content.items())
+        return content, True
 
 
 class ProofXRefRole(XRefRole):
-    def result_nodes(
-        self,
-        document: document,
-        env: BuildEnvironment,
-        node: Element,
-        is_ref: bool,
-    ) -> Tuple[List[Node], List[system_message]]:
+
+    def result_nodes(self, document: document, env: BuildEnvironment, node: Element,
+                     is_ref: bool) -> Tuple[List[Node], List[system_message]]:
 
         node["refdomain"] = "proof"
         return [node], []
@@ -46,7 +60,8 @@ class ProofDomain(Domain):
     name = "proof"
     label = "Proof Domain"
 
-    roles = {"ref": ProofXRefRole()}
+    roles = { "ref": ProofXRefRole() }
+    indices = { ProofIndex }
 
     directives = {
         "proof": ProofDirective,
@@ -62,7 +77,8 @@ class ProofDomain(Domain):
         "exercise": ExerciseDirective,
     }
 
-    def resolve_xref(self, env, fromdocname, builder, typ, target, node, contnode):
+    def resolve_xref(self, env: BuildEnvironment, fromdocname: str, builder: Builder,
+                     typ: str, target: str, node: pending_xref, contnode: Element) -> Element:
 
         try:
             match = env.proof_list[target]
