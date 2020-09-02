5403c77995591f01c10d3530d47eca4fe1584d4c
najuzilu <junaluzi@gmail.com>
2020-09-02 12:09:03 +0200

    fix pre-commit

--- tests/test_build.py
+++ tests/test_build.py
@@ -1,7 +1,7 @@
 import pytest
-import os
 
-@pytest.mark.sphinx('html', testroot='mybook')
+
+@pytest.mark.sphinx("html", testroot="mybook")
 def test_build(app):
     """Test building the book template and a few test configs."""
     app.build()
@@ -10,7 +10,7 @@ def test_build(app):
     assert (app.outdir / "proof").exists()
 
 
-@pytest.mark.sphinx('html' , testroot='missingref')
+@pytest.mark.sphinx("html", testroot="missingref")
 def test_missing_ref(app, warnings):
     # Clean build
     app.build()
